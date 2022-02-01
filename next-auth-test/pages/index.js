7fcf0e2b5f75600c8ba153c5935ec77e281a28c7
herohoro <mtwd.office.25@gmain.com>
2022-02-02 02:42:39 +0900

    setup and loginImage

--- next-auth-test/pages/index.js
+++ next-auth-test/pages/index.js
@@ -1,14 +1,37 @@
-import Link from "next/link";
 
-const Home = () => {
+import * as React from 'react';
+import { 
+  NextUIProvider,
+  Button,
+  Input,
+  Spacer,
+  Link,
+  Card,
+  Grid,
+  useTheme, Text
+
+} from '@nextui-org/react';
+
+export default function Home() {
+  const colors = [
+    'primary',
+    'secondary',
+    'success',
+    'warning',
+    'error',
+    'gradient'
+    
+  ]
+
   return (
-    <>
+    <NextUIProvider>
+      
       <h1>Home</h1>
       <div>
         <Link href="/private"><a>Privateへ</a></Link>
       </div>
-    </>
-  );
-}
+    </NextUIProvider>
+  
+  )
 
-export default Home;
+}
